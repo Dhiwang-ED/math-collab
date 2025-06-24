a00fd6a94b2267654beaed3c8ad952e6c136c042
Dhiwang-ED <awangharispa@gmail.com>
2025-06-24 22:26:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
+
+// Endpoint test
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 
 io.on('connection', socket => {
   socket.on('join', room => socket.join(room));
@@ -15,4 +21,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(5000, () => console.log('Server started on 5000'));
+// Gunakan port dari environment variable untuk Render
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server started on ${PORT}`));
